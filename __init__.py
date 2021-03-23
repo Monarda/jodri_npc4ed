42efaed217a71d34b949f4ea319500c4e20ed273
Ivan Finch <ivan.finch@stfc.ac.uk>
2021-03-23 22:28:57 +0000

    Backwards compatibility

--- __init__.py
+++ __init__.py
@@ -2,3 +2,7 @@ __all__ = ["npc4e", "magic4", ]
 
 from .src.magic4 import Magic4
 from .src.npc4e import NPC4e
+
+## Backward compatibility
+from .src.npc.buildNPC4 import BuildNPC4 as Npc4
+from .src.npc.randomNPC4 import RandomNPC4
