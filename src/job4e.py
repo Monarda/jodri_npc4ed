39dcf013297897063464f3aa714fbc9f45b5d3f0
Ivan Finch <ivan.finch@stfc.ac.uk>
2021-03-30 22:37:26 +0100

    Some documentation

--- src/job4e.py
+++ src/job4e.py
@@ -8,10 +8,13 @@ with importlib.resources.open_text(data,'jobs.json') as f:
     _job_data = json.load(f)
 
 class Job4e():
+    """ Create a job from the 4th ed GM screen booklet random roll tables """
     def __init__(self):
         pass
 
     def get(self) -> List[str]:
+        """ Return a list of who, what, and why strings"""
+        
         who  = random.choices(_job_data["who"])[0]
         what = random.choices(_job_data["what"])[0]
         why  = random.choices(_job_data["why"])[0]
