ae75f604982ae03b16a30ff32ebeb485d6b0c97e
Ivan Finch <ivan.finch@stfc.ac.uk>
2021-04-02 10:51:09 +0100

    Fixed import * issue

--- __init__.py
+++ __init__.py
@@ -1,7 +1,8 @@
-__all__ = ["npc4e", "magic4e", ]
+__all__ = ["NPC4e", "Magic4e", "Job4e"]
 
 from .src.magic4e import Magic4e
 from .src.npc4e import NPC4e
+from .src.job4e import Job4e
 
 ## Backward compatibility
 from .src.npc.buildNPC4 import BuildNPC4 as Npc4
